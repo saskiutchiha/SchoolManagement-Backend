e3c85fdf8282350cb892bbdbb0443b67e958a46b
saskiutchiha <133410537+saskiutchiha@users.noreply.github.com>
2024-12-25 01:00:47 +0100

    intialize

---
+++ src/test/java/org/ensa/schoolmanagementbackend/SchoolManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ensa.schoolmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
