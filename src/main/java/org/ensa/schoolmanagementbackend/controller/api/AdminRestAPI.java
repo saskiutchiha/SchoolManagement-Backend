cc4b91b99d2c41364317d2bde7f85089a3a914ae
YounessBoumlik <younessboumlik1949@gmail.com>
2024-12-26 04:17:13 +0100

    Admin backend

--- src/main/java/org/ensa/schoolmanagementbackend/controller/api/AdminRestAPI.java
+++ src/main/java/org/ensa/schoolmanagementbackend/controller/api/AdminRestAPI.java
@@ -1,16 +1,39 @@
 package org.ensa.schoolmanagementbackend.controller.api;
 
 import org.ensa.schoolmanagementbackend.dao.entity.Admin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ensa.schoolmanagementbackend.metier.AdminMetierImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/admin")
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AdminRestAPI {
 
-    @GetMapping("/getadmins")
-    public List<Admin> getAdmin(){
+    @Autowired
+    private AdminMetierImpl adminMetier;
 
+    @GetMapping("/getadmins")
+    public List<Admin> getAdmins(){
+        return adminMetier.getAdmins();
     }
+
+    @PostMapping("/addadmin")
+    public void addAdmin(@RequestBody Admin admin){
+        adminMetier.addAdmin(admin);
+    }
+
+    @PutMapping("/updateadmin")
+    public void updateAdmin(@RequestBody Admin admin){
+        adminMetier.updateAdmin(admin);
+    }
+
+    @DeleteMapping("deleteadmin")
+    @CrossOrigin(origins = "http://localhost:5173")
+    public void deleteAdmin(@RequestBody Admin admin){
+        adminMetier.deleteAdmin(admin);
+    }
+
 }
--- src/main/java/org/ensa/schoolmanagementbackend/metier/AdminMetierImpl.java
+++ src/main/java/org/ensa/schoolmanagementbackend/metier/AdminMetierImpl.java
@@ -1,13 +1,35 @@
 package org.ensa.schoolmanagementbackend.metier;
 
 import org.ensa.schoolmanagementbackend.dao.entity.Admin;
+import org.ensa.schoolmanagementbackend.dao.impl.AdminDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class AdminMetierImpl implements  AdminMetier {
 
+    @Autowired
+    private AdminDaoImpl adminDao;
+
     @Override
-    public List<Admin> getAdmin() {
-//        return
+    public List<Admin> getAdmins() {
+        return adminDao.getAdmins();
+    }
+
+    @Override
+    public void addAdmin(Admin admin) {
+        adminDao.addAdmin(admin);
+    }
+
+    @Override
+    public void updateAdmin(Admin admin) {
+        adminDao.updateAdmin(admin);
+    }
+
+    @Override
+    public void deleteAdmin(Admin admin) {
+        adminDao.deleteAdmin(admin);
     }
 }
